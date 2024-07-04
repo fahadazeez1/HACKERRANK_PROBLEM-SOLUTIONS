3b40865e088795288828cec17fb0c9cfe190c2c1
fahadazeez1 <azeezfahad4@gmail.com>
2024-07-05 00:39:08 +0530

    adding que&sol

---
+++ problem 018/sol.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    # Convert the string to a list to mutate it
+    string_list = list(string)
+    # Update the character at the specified position
+    string_list[position] = character
+    # Join the list back into a string and return it
+    return ''.join(string_list)
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
