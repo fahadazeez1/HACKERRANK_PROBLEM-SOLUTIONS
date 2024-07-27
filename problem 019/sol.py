29b1e347846b09b45492f3a4394906c75f47be33
FAHAD AZEEZ <147379218+fahadazeez1@users.noreply.github.com>
2024-07-27 23:48:22 +0530

    problem 19

--- problem 019/sol.py
+++ problem 019/sol.py
@@ -1,12 +1,12 @@
 # Python solution file
 def mutate_string(string, position, character):
-    # Convert the string to a list to mutate it
+    
     string_list = list(string)
     string_list[position]=character
     
-    # Update the character at the specified position
+ 
     string_list[position] = character
-    # Join the list back into a string and return it
+
     return ''.join(string_list)
 if __name__ == '__main__':
     s = input()
