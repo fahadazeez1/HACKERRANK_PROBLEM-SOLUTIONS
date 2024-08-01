dbdf3e45f212b7c2f6576f1441c394d300012861
FAHAD AZEEZ <147379218+fahadazeez1@users.noreply.github.com>
2024-08-01 22:37:04 +0530

    Update sol.py

--- problem 057/sol.py
+++ problem 057/sol.py
@@ -1,1 +1,7 @@
 # Python solution file
+ width = len(bin(number)) - 2
+
+    
+    for i in range(1, number + 1):
+        # Print the formatted values
+        print(f"{str(i).rjust(width)} {oct(i)[2:].rjust(width)} {hex(i)[2:].upper().rjust(width)} {bin(i)[2:].rjust(width)}")
