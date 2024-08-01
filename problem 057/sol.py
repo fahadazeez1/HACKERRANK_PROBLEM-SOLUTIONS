e344bcc295b445ef2bc0d4d0866a1bf4e0ef95db
FAHAD AZEEZ <147379218+fahadazeez1@users.noreply.github.com>
2024-08-01 22:37:20 +0530

    problem 57

--- problem 057/sol.py
+++ problem 057/sol.py
@@ -3,5 +3,5 @@
 
     
     for i in range(1, number + 1):
-        # Print the formatted values
+        # Print the formatted values 
         print(f"{str(i).rjust(width)} {oct(i)[2:].rjust(width)} {hex(i)[2:].upper().rjust(width)} {bin(i)[2:].rjust(width)}")
