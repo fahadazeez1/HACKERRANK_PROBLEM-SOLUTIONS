11aa14239ce330ad1ca96ec58a01f37f3ebc87d3
FAHAD AZEEZ <147379218+fahadazeez1@users.noreply.github.com>
2024-08-02 23:44:16 +0530

    Problem 58

--- problem 058/sol.py
+++ problem 058/sol.py
@@ -1,11 +1,6 @@
 # Python solution file
-# Reading input
 n, m = map(int, input().split())
 scores = [list(map(float, input().split())) for _ in range(m)]
-
-# Calculating average scores for each student
 averages = [sum(student_scores) / m for student_scores in zip(*scores)]
-
-# Printing averages, each rounded to one decimal place
 for avg in averages:
     print(f"{avg:.1f}")
