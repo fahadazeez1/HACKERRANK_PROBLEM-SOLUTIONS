476d1b03e79d059108d47f1030e801f481b2578d
fahadazeez1 <azeezfahad4@gmail.com>
2024-08-24 22:14:37 +0530

    problem 65

---
+++ problem 065/sol.py
@@ -0,0 +1,20 @@
+# Solution file
+import numpy as np
+
+# Take input from user
+A = np.array(list(map(int, input().split())))
+B = np.array(list(map(int, input().split())))
+
+# Compute inner product
+inner_product = np.inner(A, B)
+
+# Compute outer product
+outer_product = np.outer(A, B)
+
+# Print results
+print(inner_product)
+print(outer_product)
+
+
+
+s
